841f5ed0d68eba98ff40cdb8e50d9b6e8c648c55
Goutham <Gouthamsai5059@gmail.com>
2023-11-16 15:03:23 +0000

    007

--- assign/Assets/Scripts/Pausemenu.cs
+++ assign/Assets/Scripts/Pausemenu.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System.Threading.Tasks;
 
 public class Mainmenu : MonoBehaviour
 {
     [SerializeField] GameObject Pausemenu;
+    [SerializeField] RectTransform PausepanelRect;
+    [SerializeField] float topPosY, middlePosY;
+    [SerializeField] float tweenDuration;
+    [SerializeField] CanvasGroup canvasGroup; // BlackPanel canvasgroup
     public void Pause()
     {
         Pausemenu.SetActive(true);
         Time.timeScale = 0;
+        PausepanelIntro();
     }
 
-    public void Resume()
+    public async void Resume()
     {
-        Pausemenu?.SetActive(false);
+       await PausepanelOutro();
+        Pausemenu.SetActive(false);
         Time.timeScale = 1;
 
     }
@@ -35,5 +43,18 @@ public class Mainmenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+    void PausepanelIntro()
+    {
+        canvasGroup.DOFade(2, tweenDuration).SetUpdate(true);
+        PausepanelRect.DOAnchorPosY(middlePosY, tweenDuration).SetUpdate(true);
+    }
+
+    async Task PausepanelOutro()
+    {
+        canvasGroup.DOFade(0, tweenDuration).SetUpdate(true);
+
+        await PausepanelRect.DOAnchorPosY(topPosY, tweenDuration).SetUpdate(true).AsyncWaitForCompletion();
+
     }
 }
